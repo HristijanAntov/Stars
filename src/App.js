2ee32db651192655c0ffc01e3bd68ce432eb2b39
HristijanAntov <hristijan_ninja@hotmail.com>
2018-01-14 10:56:26 +0100

    Removed unneccessary commented code

--- src/App.js
+++ src/App.js
@@ -3,22 +3,6 @@ import { Route } from 'react-router-dom'
 import Home from './components/home/component'
 
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-
 const App = () =>
   <div>
     <Route exact path="/" component={Home} />
